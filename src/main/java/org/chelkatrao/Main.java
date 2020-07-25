ef8e2ecd6f3a46a85b314675c8e99ce06d944870
ashraf <chelkatrao@gmail.com>
2020-07-26 03:52:39 +0500

    first commit

--- src/main/java/org/chelkatrao/Main.java
+++ src/main/java/org/chelkatrao/Main.java
@@ -1,9 +1,11 @@
 package org.chelkatrao;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         ApiContextInitializer.init();
